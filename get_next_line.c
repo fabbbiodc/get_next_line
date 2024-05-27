1351f4e6a20a8ab20de3eb7ddd86a0e2e85d3993
Fabio Di Cecca <fdi-cecc@c4b3c6.42berlin.de>
2024-05-27 17:02:29 +0200

    start writing

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,23 @@
 /*   By: fdi-cecc <fdi-cecc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 23:22:05 by fdi-cecc          #+#    #+#             */
-/*   Updated: 2024/05/26 23:22:08 by fdi-cecc         ###   ########.fr       */
+/*   Updated: 2024/05/27 17:02:22 by fdi-cecc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	static t_list	*list = NULL;
+	char			*next_line;
+
+	if (fd <  0 || BUFFER_SIZE <= 0 || read(fd, &next_line, 0) < 0)
+		return (NULL);
+	ft_newlist(&list, fd);
+	if (list == NULL)
+		return (NULL);
+	next_line = ft_getline(list);
+	ft_cleanlist(&list);
+	return (next_line);
+}
