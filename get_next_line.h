fc4fc3686fa789b8195ebfd9ca605d873993e499
Fabio Di Cecca <fdi-cecc@c4b3c6.42berlin.de>
2024-05-27 17:02:04 +0200

    start writing

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,25 @@
 /*   By: fdi-cecc <fdi-cecc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 23:22:21 by fdi-cecc          #+#    #+#             */
-/*   Updated: 2024/05/26 23:22:22 by fdi-cecc         ###   ########.fr       */
+/*   Updated: 2024/05/27 16:42:50 by fdi-cecc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_list
+{
+	char			*content;
+	struct slist	*next;
+}				t_list;
+
+#endif
