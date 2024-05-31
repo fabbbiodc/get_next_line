64878642ffe727e5b03973bb2b364cfe2bd62f8a
Fabio Di Cecca <fdi-cecc@c4b3c6.42berlin.de>
2024-05-31 15:26:16 +0200

    copyline function

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: fdi-cecc <fdi-cecc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 23:22:21 by fdi-cecc          #+#    #+#             */
-/*   Updated: 2024/05/27 16:42:50 by fdi-cecc         ###   ########.fr       */
+/*   Updated: 2024/05/31 11:13:26 by fdi-cecc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,13 @@ typedef struct s_list
 	char			*content;
 	struct slist	*next;
 }				t_list;
+
+char	*get_next_line(int fd);
+void	ft_newlist(t_list **list, int fd);
+void	ft_newlist(t_list **list, int fd);
+void	ft_append(t_list **list, char *buffer);
+
+int	ft_newline(t_list *list);
+t_list	*ft_lastnode(t_list *list);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,11 +6,62 @@
 /*   By: fdi-cecc <fdi-cecc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 23:22:14 by fdi-cecc          #+#    #+#             */
-/*   Updated: 2024/05/30 11:54:08 by fdi-cecc         ###   ########.fr       */
+/*   Updated: 2024/05/31 15:25:51 by fdi-cecc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+void	ft_copyline(t_list *list, char *str)
+{
+	int	i;
+	int	k;
+
+	if (NULL == list)
+		return;
+	k = 0;
+	while (list)
+	{
+		i = 0;
+		while (list->content[i])
+		{
+			if (list->content[i] == '\n')
+			{
+				str[k++] = '\n';
+				str[k] = '\0';
+				return ;
+			}
+			str[k++] = list->content[i++];
+		}
+		list = list->next;
+	}
+	str[k] = '\0';
+}
+
+int	ft_linelength(t_list *list)
+{
+	int	i;
+	int	len;
+
+	if (NULL == list)
+		return (0);
+	while (list)
+	{
+		i = 0;
+		while (list->content[i])
+		{
+			if (list->content[i] == '\n')
+			{
+				len++;
+				return (len);
+			}
+			i++;
+			len++;
+		}
+		list = list->next;
+	}
+	return (len);
+}
 
 t_list	*ft_lastnode(t_list *list)
 {
