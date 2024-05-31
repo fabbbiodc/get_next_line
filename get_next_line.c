c08b0f8f5be4735d3d2f034b46270872d410f5aa
fabbbiodc <162996736+fabbbiodc@users.noreply.github.com>
2024-05-31 10:37:08 +0200

    progress

--- get_next_line.c
+++ get_next_line.c
@@ -6,18 +6,56 @@
 /*   By: fdi-cecc <fdi-cecc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 23:22:05 by fdi-cecc          #+#    #+#             */
-/*   Updated: 2024/05/27 17:02:22 by fdi-cecc         ###   ########.fr       */
+/*   Updated: 2024/05/30 16:39:41 by fdi-cecc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+void	ft_append(t_list **list, char *buffer)
+{
+	t_list	*newnode;
+	t_list	*lastnode;
+
+	lastnode = ft_lastnode(*list);
+	newnode = malloc(sizeof(t_list));
+	if (NULL == newnode)
+		return ;
+	if (NULL == lastnode)
+		*list = newnode;
+	else
+		lastnode->next = newnode;
+	newnode->content = buffer;
+	newnode->next = NULL;
+}
+
+void	ft_newlist(t_list **list, int fd)
+{
+	int		chars;
+	char	*buffer;
+
+	while (!ft_newline(list[fd]))
+	{
+		buffer = malloc(BUFFER_SIZE + 1);
+		if (NULL == buffer)
+			return ;
+		chars = read(fd, buffer, BUFFER_SIZE);
+		if (!chars)
+		{
+			free(buffer);
+			return ;
+		}
+		buffer[chars] = '\0';
+		ft_append(list, buffer);
+	}
+}
 
 char	*get_next_line(int fd)
 {
 	static t_list	*list = NULL;
 	char			*next_line;
 
-	if (fd <  0 || BUFFER_SIZE <= 0 || read(fd, &next_line, 0) < 0)
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &next_line, 0) < 0)
 		return (NULL);
 	ft_newlist(&list, fd);
 	if (list == NULL)
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,35 @@
 /*   By: fdi-cecc <fdi-cecc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 23:22:14 by fdi-cecc          #+#    #+#             */
-/*   Updated: 2024/05/27 17:02:25 by fdi-cecc         ###   ########.fr       */
+/*   Updated: 2024/05/30 11:54:08 by fdi-cecc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+t_list	*ft_lastnode(t_list *list)
+{
+	if (NULL == list)
+		return (NULL);
+	while (list->next)
+		list = list->next;
+	return (list);
+}
+
+int	ft_newline(t_list *list)
+{
+	int	i;
+
+	if (NULL == list)
+		return (0);
+	while (list)
+	{
+		while (list->content[i] && i < BUFFER_SIZE)
+		{
+			if (list->content[i] == '\n')
+				return (1);
+			i++;
+		}
+	}
+	list = list->next;
+}
