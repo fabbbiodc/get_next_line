123c988485dffb1f1f99f403a1db15c0b3cf534b
fabbbiodc <162996736+fabbbiodc@users.noreply.github.com>
2024-06-06 13:33:33 +0200

    working on tester

--- test_bonus.c
+++ test_bonus.c
@@ -6,7 +6,7 @@
 /*   By: fdi-cecc <fdi-cecc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 18:35:10 by fdi-cecc          #+#    #+#             */
-/*   Updated: 2024/06/04 19:22:13 by fdi-cecc         ###   ########.fr       */
+/*   Updated: 2024/06/06 12:05:19 by fdi-cecc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,40 +16,30 @@ int main(int argc, char **argv)
 {
 	int i;
 	int fd[argc];
-
+	char *line[argc];
 	i = 0;
 
 	while (i < argc)
 	{
-		fd[i++] = open(argv[i], O_RDONLY);
+		fd[i] = open(argv[i], O_RDONLY);
 		if (fd[i] < 0)
 			perror("Check your FDs");
+		i++;
 	}
 	i = 0;
-	
-
-
-		
-		
-		
-		
-		
-		
-		
-		if (fd == -1)
+	while (i < argc)
+	{
+		line[i] = get_next_line(fd[i]);
+		while (line[i])
 		{
-			perror("Error opening file");
-			return 1;
+			if (line[i])
+			{
+				printf("%s", line[i]);
+				free(line[i]);
+			}
+			printf("\n");
 		}
-
-		char *line;
-		while ((line = get_next_line(fd)) != NULL)
-		{
-			printf("%s", line);
-			free(line);
-		}
-	close(fd);
+		i++;
 	}
-    return 0;
-
+	return (0);
 }
