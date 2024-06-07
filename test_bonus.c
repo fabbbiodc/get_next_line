1951c4f8e02c578e7f0ab68e0bbe7fe4b9110538
fabbbiodc <162996736+fabbbiodc@users.noreply.github.com>
2024-06-07 09:40:38 +0200

    tester

--- test_bonus.c
+++ test_bonus.c
@@ -6,7 +6,7 @@
 /*   By: fdi-cecc <fdi-cecc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 18:35:10 by fdi-cecc          #+#    #+#             */
-/*   Updated: 2024/06/06 12:05:19 by fdi-cecc         ###   ########.fr       */
+/*   Updated: 2024/06/06 17:03:53 by fdi-cecc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,119 @@
 
 int main(int argc, char **argv)
 {
+    int i;
+    int *fd;
+    char **lines;
+    int *file_done;
+    int files_remaining;
+
+    if (argc < 2)
+    {
+        printf("Specify the files please: %s file1 file2 ...\n", argv[0]);
+        return 1;
+    }
+
+    // Allocate memory for file descriptors, lines, and file status
+    fd = (int *)malloc((argc - 1) * sizeof(int));
+    lines = (char **)malloc((argc - 1) * sizeof(char *));
+    file_done = (int *)malloc((argc - 1) * sizeof(int));
+
+    for (i = 0; i < argc - 1; i++)
+    {
+        fd[i] = open(argv[i + 1], O_RDONLY);
+        if (fd[i] < 0)
+        {
+            perror("Error opening file");
+            free(fd);
+            free(lines);
+            free(file_done);
+            return 1;
+        }
+        lines[i] = NULL;
+        file_done[i] = 0; // Initialize file status as not done
+    }
+
+    files_remaining = argc - 1;
+    while (files_remaining > 0)
+    {
+        for (i = 0; i < argc - 1; i++)
+        {
+            if (!file_done[i])
+            {
+                if (lines[i] != NULL)
+                {
+                    free(lines[i]);
+                }
+                lines[i] = get_next_line(fd[i]);
+                if (lines[i] != NULL)
+                {
+                    printf("%s", lines[i]);
+                }
+                else
+                {
+                    file_done[i] = 1; // Mark this file as done
+                    files_remaining--;
+                }
+            }
+        }
+    }
+
+    for (i = 0; i < argc - 1; i++)
+    {
+        close(fd[i]);
+        if (lines[i] != NULL)
+        {
+            free(lines[i]);
+        }
+    }
+
+    free(fd);
+    free(lines);
+    free(file_done);
+
+    return 0;
+}
+
+
+/* int main(int argc, char **argv)
+{
 	int i;
-	int fd[argc];
-	char *line[argc];
-	i = 0;
+	int fd[argc - 1];
+	char *line;
+	i = 1;
+
+	   if (argc < 2)
+    {
+        printf("Specify the files please: %s file1 file2 ...\n", argv[0]);
+        return (1);
+    }
 
 	while (i < argc)
 	{
-		fd[i] = open(argv[i], O_RDONLY);
-		if (fd[i] < 0)
-			perror("Check your FDs");
-		i++;
-	}
-	i = 0;
-	while (i < argc)
-	{
-		line[i] = get_next_line(fd[i]);
-		while (line[i])
+		fd[i - 1] = open(argv[i], O_RDONLY);
+		if (fd[i - 1] < 0)
 		{
-			if (line[i])
-			{
-				printf("%s", line[i]);
-				free(line[i]);
-			}
-			printf("\n");
+			perror("Check your FDs");
+			return (1);
 		}
 		i++;
 	}
+	i = 0;
+	while (i < argc - 1)
+	{
+		while ((line = get_next_line(fd[i])) != NULL)
+		{
+			if (line[i])
+			{
+				printf("%s", line);
+				free(line);
+			}
+			printf("\n");
+		}
+		close(fd[i]);
+		i++;
+	}
 	return (0);
-}
+}  */
+
+//cc -Wall -Werror -Wextra test_bonus.c get_next_line_bonus.c get_next_line_utils_bonus.c -o test_bonus
